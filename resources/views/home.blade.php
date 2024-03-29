b4e436fba79b23b58c81f6e44f38fea3660f0409
Abdulrehman-8961 <danimughal8961@gmail.com>
2024-03-29 17:13:03 +0500

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -444,66 +444,68 @@
             });
 
             function exportToCsv(filename, rows) {
-        var processRow = function (row) {
-            var finalVal = '';
-            for (var j = 0; j < row.length; j++) {
-                var innerValue = row[j] === null ? '' : row[j].toString();
-                if (row[j] instanceof Date) {
-                    innerValue = row[j].toLocaleString();
+                var processRow = function(row) {
+                    var finalVal = '';
+                    for (var j = 0; j < row.length; j++) {
+                        var innerValue = row[j] === null ? '' : row[j].toString();
+                        if (row[j] instanceof Date) {
+                            innerValue = row[j].toLocaleString();
+                        }
+                        var result = innerValue.replace(/"/g, '""');
+                        if (result.search(/("|,|\n)/g) >= 0)
+                            result = '"' + result + '"';
+                        if (j > 0)
+                            finalVal += ',';
+                        finalVal += result;
+                    }
+                    return finalVal + '\n';
+                };
+
+                var csvFile = '';
+
+                // Add table headers
+                var tableHeaders = [];
+                $('.search-table thead th').each(function() {
+                    tableHeaders.push($(this).text());
+                });
+                csvFile += processRow(tableHeaders);
+
+                // Add table data
+                for (var i = 0; i < rows.length; i++) {
+                    csvFile += processRow(rows[i]);
                 }
-                var result = innerValue.replace(/"/g, '""');
-                if (result.search(/("|,|\n)/g) >= 0)
-                    result = '"' + result + '"';
-                if (j > 0)
-                    finalVal += ',';
-                finalVal += result;
+
+                var blob = new Blob([csvFile], {
+                    type: 'text/csv;charset=utf-8;'
+                });
+                if (navigator.msSaveBlob) { // IE 10+
+                    navigator.msSaveBlob(blob, filename);
+                } else {
+                    var link = document.createElement("a");
+                    if (link.download !== undefined) {
+                        var url = URL.createObjectURL(blob);
+                        link.setAttribute("href", url);
+                        link.setAttribute("download", filename);
+                        link.style.visibility = 'hidden';
+                        document.body.appendChild(link);
+                        link.click();
+                        document.body.removeChild(link);
+                    }
+                }
             }
-            return finalVal + '\n';
-        };
 
-        var csvFile = '';
-
-        // Add table headers
-        var tableHeaders = [];
-        $('.search-table thead th').each(function() {
-            tableHeaders.push($(this).text());
-        });
-        csvFile += processRow(tableHeaders);
-
-        // Add table data
-        for (var i = 0; i < rows.length; i++) {
-            csvFile += processRow(rows[i]);
-        }
-
-        var blob = new Blob([csvFile], { type: 'text/csv;charset=utf-8;' });
-        if (navigator.msSaveBlob) { // IE 10+
-            navigator.msSaveBlob(blob, filename);
-        } else {
-            var link = document.createElement("a");
-            if (link.download !== undefined) {
-                var url = URL.createObjectURL(blob);
-                link.setAttribute("href", url);
-                link.setAttribute("download", filename);
-                link.style.visibility = 'hidden';
-                document.body.appendChild(link);
-                link.click();
-                document.body.removeChild(link);
-            }
-        }
-    }
-
-    // Event listener for Export CSV button
-    $('#exportCsvBtn').on('click', function() {
-        var rows = [];
-        $('.search-table tbody tr').each(function(index, tr) {
-            var rowData = [];
-            $(tr).find('td').each(function(index, td) {
-                rowData.push($(td).text());
+            // Event listener for Export CSV button
+            $('#exportCsvBtn').on('click', function() {
+                var rows = [];
+                $('.search-table tbody tr').each(function(index, tr) {
+                    var rowData = [];
+                    $(tr).find('td').each(function(index, td) {
+                        rowData.push($(td).text());
+                    });
+                    rows.push(rowData);
+                });
+                exportToCsv('data.csv', rows);
             });
-            rows.push(rowData);
-        });
-        exportToCsv('data.csv', rows);
-    });
 
             // Event listeners for buttons
             prevBtn.addEventListener('click', function() {
